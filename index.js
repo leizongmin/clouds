e86695517b906919fb4fee99105784bb9906f367
Lei Zongmin <leizongmin@gmail.com>
2012-09-05 15:41:06 +0800

    first

---
+++ index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * cloud
+ *
+ * @author 老雷<leizongmin@gmail.com>
+ */
+
+
+var exports = module.exports = require('./lib/cloud');
+
+exports.version = require('./package.json').version;
---
+++ simple/test.js
@@ -0,0 +1,40 @@
+'use strict';
+
+var cloud = require('cloud');
+
+cloud.connect({
+  redis: {
+    db:     4,
+    prefix: 'TEST:'
+  }
+});
+
+cloud.register('hello', function (err, service) {
+  if (err) throw err;
+  //console.log(service);
+  //cloud.unregister('hello', function (err) {
+  //  if (err) throw err;
+  //  process.exit();
+  //});
+  
+  service.on('say', function (msg, callback) {
+    console.log('on say: ' + msg);
+    callback(null, 'ok');
+  });
+
+  var hello = cloud.require('hello2');
+  hello.emit('say', 'first');
+  hello.emit('say', 'second', function (err, ret) {
+    if (err) console.log(err.stack);
+    console.log('emit say: ' + ret);
+  });
+  
+  die(1000);
+});
+
+
+function die (t) {
+  setTimeout(function () {
+    process.exit();
+  }, t);
+}
