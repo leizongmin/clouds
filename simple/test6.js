2a57a2ae1378298a75833d5eed07c522a6a94686
Lei Zongmin <leizongmin@gmail.com>
2012-09-06 18:23:01 +0800

    若 service.emit() 时没有指定回调函数，则不会修改进程的分数

---
+++ simple/test6.js
@@ -0,0 +1,39 @@
+'use strict';
+
+var clouds = require('clouds');
+
+clouds.connect({
+  redis: {
+    db:     4,
+    prefix: 'TEST:'
+  },
+  service: {
+    callbackTimeout:  1000
+  }
+});
+
+clouds.register('test6', function (err, service) {
+  if (err) throw err;
+
+  service.on('say', function (msg, callback) {
+    console.log(' on say: ' + msg);
+    //callback(null);
+  });
+
+  var s = clouds.require('test6');
+  
+  for (var i = 0; i < 10; i++) {
+    setTimeout(function () {
+      s.emit('say', 'hello ' + i);
+      //s.emit('say', 'hello ' + i, function () { });
+    }, i * 1000)
+  }
+  die(20000);
+});
+
+
+function die (t) {
+  setTimeout(function () {
+    process.exit();
+  }, t);
+}
