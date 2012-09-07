0da9b55f58427dafce895a80f0025294d9e75cbe
Lei Zongmin <leizongmin@gmail.com>
2012-09-07 14:11:20 +0800

    封装Wind.js的异步方法，比如可直接这样使用： var service = $await(clouds.register('服务名'));

--- index.js
+++ index.js
@@ -6,7 +6,51 @@
  * @author 老雷<leizongmin@gmail.com>
  */
 
+var debug = require('debug')('cloud');
+var cloud = exports = module.exports = require('./lib/cloud');
 
-var exports = module.exports = require('./lib/cloud');
+cloud.version = require('./package.json').version;
 
-exports.version = require('./package.json').version;
+
+/**
+ * 使用Wind.js封装异步方法
+ * 返回当前使用的wind.js对象
+ *
+ * @param {int} loggerLevel
+ * @return {object}
+ */
+cloud.wind = function (loggerLevel) {
+  if (cloud._wind) {
+    if (isFinite(loggerLevel)) cloud._wind.logger.level = loggerLevel;
+    return cloud._wind;
+  }
+
+  var Wind = require('wind');
+  var bind = Wind.Async.Binding.fromStandard;
+  
+  loggerLevel = isFinite(loggerLevel) ? loggerLevel : Wind.Logging.Level.WARN;
+  Wind.logger.level = loggerLevel;
+  
+  cloud.registerAsync = bind(cloud.register);
+  cloud.unregisterAsync = bind(cloud.unregister);
+  cloud.cleanAsync = bind(cloud.clean);
+  cloud.heartbeatAsync = bind(cloud.heartbeat);
+  
+  var Service = cloud.Service.prototype;
+  Service.relatedProcessAsync = bind(Service.relatedProcess);
+  Service.heartbeatAsync = bind(Service.heartbeat);
+  Service.getAsync = bind(Service.get);
+  Service.setAsync = bind(Service.set);
+  Service.removeAsync = bind(Service.remove);
+  
+  var RemoteService = cloud.RemoteService.prototype;
+  RemoteService.emitAsync = bind(RemoteService.emit);
+  RemoteService.pemitAsync = bind(RemoteService.pemit);
+  RemoteService.relatedProcessAsync = bind(RemoteService.relatedProcess);
+  RemoteService.setAsync = bind(RemoteService.set);
+  RemoteService.getAsync = bind(RemoteService.get);
+  RemoteService.removeAsync = bind(RemoteService.remove);
+  
+  cloud._wind = Wind;
+  return Wind;
+};
