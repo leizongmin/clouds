f391e1b2fc509165755d92ab04a80379c98532a3
Zongmin Lei <leizongmin@gmail.com>
2014-12-05 19:20:21 +0800

    增加调用1000次的测试

--- test/test_multi_server.js
+++ test/test_multi_server.js
@@ -115,4 +115,52 @@ describe('multi', function () {
     });
   });
 
+  it('test6 - 1000 calls', function (done) {
+    var s1 = clouds.createServer();
+    var s2 = clouds.createServer();
+    var s3 = clouds.createServer();
+    var c = clouds.createClient({timeout: 20});
+
+    var MAX = 1000;
+    var counter = {a: 0, b: 0, c: 0};
+    var multi1 = c.bind('test6.multi.1', 1);
+
+    async.series([
+      function (next) {
+        s1.register('test6.multi.1', function (v, callback) {
+          callback(null, 'a');
+        }, next);
+      },
+      function (next) {
+        s2.register('test6.multi.1', function (v, callback) {
+          callback(null, 'b');
+        }, next);
+      },
+      function (next) {
+        s3.register('test6.multi.1', function (v, callback) {
+          callback(null, 'c');
+        }, next);
+      },
+      function (next) {
+        async.timesSeries(MAX, function (i, next) {
+          multi1(i, function (err, n) {
+            should.equal(err, null);
+            counter[n]++;
+            next();
+          });
+        }, next);
+      }
+    ], function (err) {
+      console.log(err && err.stack);
+      should.equal(err, null);
+      (counter.a + counter.b + counter.c).should.equal(MAX);
+      checkMessagesClean(c);
+      s1.exit();
+      s2.exit();
+      s3.exit();
+      c.exit();
+      done();
+    });
+  });
+
 });
