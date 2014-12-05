94b392cb05ff1ebcea79836587c4687d661b3332
Zongmin Lei <leizongmin@gmail.com>
2014-12-05 14:04:20 +0800

    调用超时，指定自定义onRetry函数，测试代码

--- test/test_timeout.js
+++ test/test_timeout.js
@@ -88,4 +88,43 @@ describe('timeout', function () {
     });
   });
 
+  it('test3 - retry & assigned onRetry', function (done) {
+    var s = clouds.createServer();
+    var c = clouds.createClient({timeout: 1});
+
+    var timeout4 = c.bind('test3.timeout.4', 1, function (v, callback) {
+      v.should.equal(arg1);
+      counter1++;
+      callback(v + 1);
+    });
+    var counter1 = 0;
+    var counter2 = 0;
+    var arg1 = parseInt(Math.random() * 10, 10);
+
+    async.series([
+      function (next) {
+        s.register('test3.timeout.4', function (v, callback) {
+          v.should.equal(arg1 + 1);
+          counter2++;
+          setTimeout(callback, 1500);
+        }, next);
+      },
+      function (next) {
+        timeout4(arg1, function (err, ret) {
+          should.notEqual(err, null);
+          err.code.should.equal('CLOUDS_CALL_SERVICE_TIMEOUT');
+          counter1.should.equal(2);
+          counter2.should.equal(2);
+          next();
+        });
+      },
+    ], function (err) {
+      console.log(err && err.stack);
+      should.equal(err, null);
+      s.exit();
+      c.exit();
+      done();
+    });
+  });
+
 });
