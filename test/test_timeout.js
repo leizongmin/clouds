dd887b2818580bd15bd7cdff5b7850fbba2fc969
Zongmin Lei <leizongmin@gmail.com>
2014-12-05 17:33:53 +0800

    当调用不成功时，可自动清理远程服务器列表

--- test/test_timeout.js
+++ test/test_timeout.js
@@ -59,7 +59,10 @@ describe('timeout', function () {
 
   it('test3 - retry', function (done) {
     var s = clouds.createServer();
-    var c = clouds.createClient({timeout: 1});
+    var c = clouds.createClient({
+      timeout: 1,
+      notAutoCleanRemoteServer: true
+    });
 
     var timeout3 = c.bind('test3.timeout.3', 3);
     var counter = 0;
@@ -73,6 +76,7 @@ describe('timeout', function () {
       },
       function (next) {
         timeout3(function (err, ret) {
+          console.log(arguments);
           should.notEqual(err, null);
           err.code.should.equal('CLOUDS_CALL_SERVICE_TIMEOUT');
           counter.should.equal(4);
@@ -90,7 +94,10 @@ describe('timeout', function () {
 
   it('test3 - retry & assigned onRetry', function (done) {
     var s = clouds.createServer();
-    var c = clouds.createClient({timeout: 1});
+    var c = clouds.createClient({
+      timeout: 1,
+      notAutoCleanRemoteServer: true
+    });
 
     var timeout4 = c.bind('test3.timeout.4', 1, function (v, callback) {
       v.should.equal(arg1);
@@ -115,6 +122,47 @@ describe('timeout', function () {
           err.code.should.equal('CLOUDS_CALL_SERVICE_TIMEOUT');
           counter1.should.equal(2);
           counter2.should.equal(2);
+          next();
+        });
+      },
+    ], function (err) {
+      console.log(err && err.stack);
+      should.equal(err, null);
+      s.exit();
+      c.exit();
+      done();
+    });
+  });
+
+  it('test3 - retry & assigned onRetry', function (done) {
+    var s = clouds.createServer();
+    var c = clouds.createClient({
+      timeout: 1
+    });
+
+    var timeout5 = c.bind('test3.timeout.5', 1, function (v, callback) {
+      v.should.equal(arg1);
+      counter1++;
+      callback(v + 1);
+    });
+    var counter1 = 0;
+    var counter2 = 0;
+    var arg1 = parseInt(Math.random() * 10, 10);
+
+    async.series([
+      function (next) {
+        s.register('test3.timeout.5', function (v, callback) {
+          v.should.equal(arg1 + 1);
+          counter2++;
+          setTimeout(callback, 1500);
+        }, next);
+      },
+      function (next) {
+        timeout5(arg1, function (err, ret) {
+          should.notEqual(err, null);
+          err.code.should.equal('CLOUDS_NO_AVAILABLE_SERVER');
+          counter1.should.equal(2);
+          counter2.should.equal(1);
           next();
         });
       },
