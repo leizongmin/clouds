31e724b32ba0009cffd06b40d4a4786b21b57fd0
Zongmin Lei <leizongmin@gmail.com>
2014-12-15 17:10:55 +0800

    Client和Server均继承自Base

--- test/test_base.js
+++ test/test_base.js
@@ -1,6 +1,7 @@
 var clouds = require('../');
 var should = require('should');
 var async = require('async');
+var EventProxy = require('eventproxy');
 
 describe('register & call', function () {
 
@@ -12,94 +13,104 @@ describe('register & call', function () {
     var s = clouds.createServer();
     var c = clouds.createClient({timeout: 5});
 
-    var checkCallback = {
-      step1: false,
-      step2: false,
-      step3: false,
-      step4: false,
-      step5: false,
-      step6: false
-    };
+    var ep = EventProxy.create('s', 'c', function () {
 
-    async.series([
-      function (next) {
-        s.register('test1.args.1', function (a, callback) {
-          callback(null, a + 1);
-        }, next);
-      },
-      function (next) {
-        s.register('test1.args.2', function (a, b, callback) {
-          callback(null, a + b);
-        }, next);
-      },
-      function (next) {
-        s.register('test1.args.3', function (a, b, c, callback) {
-          callback(null, a + b + c);
-        }, next);
-      },
-      function (next) {
-        c.call('test1.args.1', [1], function (err, ret) {
-          should.equal(err, null);
-          ret.should.equal(2);
-          checkCallback.step1 = true;
-          next();
-        });
-      },
-      function (next) {
-        c.call('test1.args.1', [5], function (err, ret) {
-          should.equal(err, null);
-          ret.should.equal(6);
-          checkCallback.step2 = true;
-          next();
-        });
-      },
-      function (next) {
-        c.call('test1.args.2', [1, 2], function (err, ret) {
-          should.equal(err, null);
-          ret.should.equal(3);
-          checkCallback.step3 = true;
-          next();
-        });
-      },
-      function (next) {
-        c.call('test1.args.2', [4, 5], function (err, ret) {
-          should.equal(err, null);
-          ret.should.equal(9);
-          checkCallback.step4 = true;
-          next();
-        });
-      },
-      function (next) {
-        c.call('test1.args.3', [1, 2, 3], function (err, ret) {
-          should.equal(err, null);
-          ret.should.equal(6);
-          checkCallback.step5 = true;
-          next();
-        });
-      },
-      function (next) {
-        c.call('test1.args.3', [4, 5, 6], function (err, ret) {
-          should.equal(err, null);
-          ret.should.equal(15);
-          checkCallback.step6 = true;
-          next();
-        });
-      }
-    ], function (err) {
-      console.log(err && err.stack);
-      should.equal(err, null);
+      var checkCallback = {
+        step1: false,
+        step2: false,
+        step3: false,
+        step4: false,
+        step5: false,
+        step6: false
+      };
 
-      checkCallback.step1.should.equal(true);
-      checkCallback.step2.should.equal(true);
-      checkCallback.step3.should.equal(true);
-      checkCallback.step4.should.equal(true);
-      checkCallback.step5.should.equal(true);
-      checkCallback.step6.should.equal(true);
+      async.series([
+        function (next) {
+          s.register('test1.args.1', function (a, callback) {
+            callback(null, a + 1);
+          }, next);
+        },
+        function (next) {
+          s.register('test1.args.2', function (a, b, callback) {
+            callback(null, a + b);
+          }, next);
+        },
+        function (next) {
+          s.register('test1.args.3', function (a, b, c, callback) {
+            callback(null, a + b + c);
+          }, next);
+        },
+        function (next) {
+          c.call('test1.args.1', [1], function (err, ret) {
+            should.equal(err, null);
+            ret.should.equal(2);
+            checkCallback.step1 = true;
+            next();
+          });
+        },
+        function (next) {
+          c.call('test1.args.1', [5], function (err, ret) {
+            should.equal(err, null);
+            ret.should.equal(6);
+            checkCallback.step2 = true;
+            next();
+          });
+        },
+        function (next) {
+          c.call('test1.args.2', [1, 2], function (err, ret) {
+            should.equal(err, null);
+            ret.should.equal(3);
+            checkCallback.step3 = true;
+            next();
+          });
+        },
+        function (next) {
+          c.call('test1.args.2', [4, 5], function (err, ret) {
+            should.equal(err, null);
+            ret.should.equal(9);
+            checkCallback.step4 = true;
+            next();
+          });
+        },
+        function (next) {
+          c.call('test1.args.3', [1, 2, 3], function (err, ret) {
+            should.equal(err, null);
+            ret.should.equal(6);
+            checkCallback.step5 = true;
+            next();
+          });
+        },
+        function (next) {
+          c.call('test1.args.3', [4, 5, 6], function (err, ret) {
+            should.equal(err, null);
+            ret.should.equal(15);
+            checkCallback.step6 = true;
+            next();
+          });
+        }
+      ], function (err) {
+        console.log(err && err.stack);
+        should.equal(err, null);
 
-      checkMessagesClean(c);
-      s.exit();
-      c.exit();
-      done();
+        checkCallback.step1.should.equal(true);
+        checkCallback.step2.should.equal(true);
+        checkCallback.step3.should.equal(true);
+        checkCallback.step4.should.equal(true);
+        checkCallback.step5.should.equal(true);
+        checkCallback.step6.should.equal(true);
+
+        checkMessagesClean(c);
+        s.exit();
+        c.exit();
+        done();
+      });
+
+    });
+    c.on('listen', function () {
+      ep.emit('c');
+    });
+    s.on('listen', function () {
+      ep.emit('s');
     });
   });
 
@@ -107,98 +118,108 @@ describe('register & call', function () {
     var s = clouds.createServer();
     var c = clouds.createClient({timeout: 5});
 
-    var arg1 = c.bind('test2.args.1');
-    var arg2 = c.bind('test2.args.2');
-    var arg3 = c.bind('test2.args.3');
+    var ep = EventProxy.create('s', 'c', function () {
 
-    var checkCallback = {
-      step1: false,
-      step2: false,
-      step3: false,
-      step4: false,
-      step5: false,
-      step6: false
-    };
+      var arg1 = c.bind('test2.args.1');
+      var arg2 = c.bind('test2.args.2');
+      var arg3 = c.bind('test2.args.3');
 
-    async.series([
-      function (next) {
-        s.register('test2.args.1', function (a, callback) {
-          callback(null, a + 1);
-        }, next);
-      },
-      function (next) {
-        s.register('test2.args.2', function (a, b, callback) {
-          callback(null, a + b);
-        }, next);
-      },
-      function (next) {
-        s.register('test2.args.3', function (a, b, c, callback) {
-          callback(null, a + b + c);
-        }, next);
-      },
-      function (next) {
-        arg1(1, function (err, ret) {
-          should.equal(err, null);
-          ret.should.equal(2);
-          checkCallback.step1 = true;
-          next();
-        });
-      },
-      function (next) {
-        arg1(5, function (err, ret) {
-          should.equal(err, null);
-          ret.should.equal(6);
-          checkCallback.step2 = true;
-          next();
-        });
-      },
-      function (next) {
-        arg2(1, 2, function (err, ret) {
-          should.equal(err, null);
-          ret.should.equal(3);
-          checkCallback.step3 = true;
-          next();
-        });
-      },
-      function (next) {
-        arg2(4, 5, function (err, ret) {
-          should.equal(err, null);
-          ret.should.equal(9);
-          checkCallback.step4 = true;
-          next();
-        });
-      },
-      function (next) {
-        arg3(1, 2, 3, function (err, ret) {
-          should.equal(err, null);
-          ret.should.equal(6);
-          checkCallback.step5 = true;
-          next();
-        });
-      },
-      function (next) {
-        arg3(4, 5, 6, function (err, ret) {
-          should.equal(err, null);
-          ret.should.equal(15);
-          checkCallback.step6 = true;
-          next();
-        });
-      }
-    ], function (err) {
-      console.log(err && err.stack);
-      should.equal(err, null);
+      var checkCallback = {
+        step1: false,
+        step2: false,
+        step3: false,
+        step4: false,
+        step5: false,
+        step6: false
+      };
 
-      checkCallback.step1.should.equal(true);
-      checkCallback.step2.should.equal(true);
-      checkCallback.step3.should.equal(true);
-      checkCallback.step4.should.equal(true);
-      checkCallback.step5.should.equal(true);
-      checkCallback.step6.should.equal(true);
+      async.series([
+        function (next) {
+          s.register('test2.args.1', function (a, callback) {
+            callback(null, a + 1);
+          }, next);
+        },
+        function (next) {
+          s.register('test2.args.2', function (a, b, callback) {
+            callback(null, a + b);
+          }, next);
+        },
+        function (next) {
+          s.register('test2.args.3', function (a, b, c, callback) {
+            callback(null, a + b + c);
+          }, next);
+        },
+        function (next) {
+          arg1(1, function (err, ret) {
+            should.equal(err, null);
+            ret.should.equal(2);
+            checkCallback.step1 = true;
+            next();
+          });
+        },
+        function (next) {
+          arg1(5, function (err, ret) {
+            should.equal(err, null);
+            ret.should.equal(6);
+            checkCallback.step2 = true;
+            next();
+          });
+        },
+        function (next) {
+          arg2(1, 2, function (err, ret) {
+            should.equal(err, null);
+            ret.should.equal(3);
+            checkCallback.step3 = true;
+            next();
+          });
+        },
+        function (next) {
+          arg2(4, 5, function (err, ret) {
+            should.equal(err, null);
+            ret.should.equal(9);
+            checkCallback.step4 = true;
+            next();
+          });
+        },
+        function (next) {
+          arg3(1, 2, 3, function (err, ret) {
+            should.equal(err, null);
+            ret.should.equal(6);
+            checkCallback.step5 = true;
+            next();
+          });
+        },
+        function (next) {
+          arg3(4, 5, 6, function (err, ret) {
+            should.equal(err, null);
+            ret.should.equal(15);
+            checkCallback.step6 = true;
+            next();
+          });
+        }
+      ], function (err) {
+        console.log(err && err.stack);
+        should.equal(err, null);
 
-      checkMessagesClean(c);
-      s.exit();
-      c.exit();
-      done();
+        checkCallback.step1.should.equal(true);
+        checkCallback.step2.should.equal(true);
+        checkCallback.step3.should.equal(true);
+        checkCallback.step4.should.equal(true);
+        checkCallback.step5.should.equal(true);
+        checkCallback.step6.should.equal(true);
+
+        checkMessagesClean(c);
+        s.exit();
+        c.exit();
+        done();
+      });
+
+    });
+    c.on('listen', function () {
+      ep.emit('c');
+    });
+    s.on('listen', function () {
+      ep.emit('s');
     });
   });
 
