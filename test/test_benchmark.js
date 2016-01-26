68ade115191e09ec65dbd06c9b6e0a2b9f3209f0
Zongmin Lei <leizongmin@gmail.com>
2016-01-26 15:48:35 +0800

    改进benchmark

--- test/test_benchmark.js
+++ test/test_benchmark.js
@@ -1,6 +1,8 @@
-var clouds = require('../');
-var should = require('should');
-var async = require('async');
+'use strict';
+
+const clouds = require('../');
+const should = require('should');
+const async = require('async');
 
 describe('benchmark', function () {
 
@@ -9,33 +11,39 @@ describe('benchmark', function () {
   }
 
   it('test7 - 10000 calls', function (done) {
-    var s1 = clouds.createServer();
-    var s2 = clouds.createServer();
-    var s3 = clouds.createServer();
-    var c = clouds.createClient({timeout: 20});
+    const s1 = clouds.createServer();
+    const s2 = clouds.createServer();
+    const s3 = clouds.createServer();
+    const c = clouds.createClient({timeout: 20});
 
-    var MAX = 10000;
-    var counter = {a: 0, b: 0, c: 0};
-    var multi1 = c.bind('test7.multi.1', 1);
+    const MAX = 10000;
+    let counter = {a: 0, b: 0, c: 0};
+    const multi1 = c.bind('test7.multi.1', 1);
 
     async.series([
       function (next) {
         s1.register('test7.multi.1', function (v, callback) {
-          callback(null, 'a');
+          setTimeout(function () {
+            callback(null, 'a');
+          }, Math.random() * 200);
         }, next);
       },
       function (next) {
         s2.register('test7.multi.1', function (v, callback) {
-          callback(null, 'b');
+          setTimeout(function () {
+            callback(null, 'b');
+          }, Math.random() * 200);
         }, next);
       },
       function (next) {
         s3.register('test7.multi.1', function (v, callback) {
-          callback(null, 'c');
+          setTimeout(function () {
+            callback(null, 'c');
+          }, Math.random() * 200);
         }, next);
       },
       function (next) {
-        async.timesSeries(MAX, function (i, next) {
+        async.times(MAX, function (i, next) {
           multi1(i, function (err, n) {
             should.equal(err, null);
             counter[n]++;
