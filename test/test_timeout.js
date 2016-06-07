17fe85122d18e11831cacf528f83a9563b18d48f
Zongmin Lei <leizongmin@gmail.com>
2016-06-07 17:34:03 +0800

    client options: timeoutChecker

--- test/test_timeout.js
+++ test/test_timeout.js
@@ -39,6 +39,102 @@ describe('timeout', function () {
     });
   });
 
+  it('test3 - normal call - timeoutChecker=0.5', function (done) {
+    const s = clouds.createServer();
+    const c = clouds.createClient({timeout: 1, timeoutChecker: 0.5});
+    async.series([
+      function (next) {
+        c.bind('test3.timeout.1');
+        c.ready(next);
+      },
+      function (next) {
+        s.register('test3.timeout.1', function (callback) {
+          setTimeout(callback, 2000);
+        }, next);
+      },
+      function (next) {
+        c.call('test3.timeout.1', [], function (err, ret) {
+          assert.notEqual(err, null);
+          assert.equal(err.code, 'CLOUDS_CALL_SERVICE_TIMEOUT');
+          next();
+        });
+      },
+    ], function (err) {
+      console.log(err && err.stack);
+      assert.equal(err, null);
+      Promise.all([s.exitP(), c.exitP()]).then(ret => done()).catch(done);
+    });
+  });
+
+  it('test3 - normal call - timeoutChecker=1', function (done) {
+    const s = clouds.createServer();
+    const c = clouds.createClient({timeout: 1, timeoutChecker: 1});
+    async.series([
+      function (next) {
+        c.bind('test3.timeout.1');
+        c.ready(next);
+      },
+      function (next) {
+        s.register('test3.timeout.1', function (callback) {
+          setTimeout(callback, 2000);
+        }, next);
+      },
+      function (next) {
+        c.call('test3.timeout.1', [], function (err, ret) {
+          assert.notEqual(err, null);
+          assert.equal(err.code, 'CLOUDS_CALL_SERVICE_TIMEOUT');
+          next();
+        });
+      },
+    ], function (err) {
+      console.log(err && err.stack);
+      assert.equal(err, null);
+      Promise.all([s.exitP(), c.exitP()]).then(ret => done()).catch(done);
+    });
+  });
+
+  it('test3 - normal call - no server', function (done) {
+    const c = clouds.createClient({timeout: 1});
+    async.series([
+      function (next) {
+        c.bind('test3.timeout.1');
+        c.ready(next);
+      },
+      function (next) {
+        c.call('test3.timeout.1', [], function (err, ret) {
+          assert.notEqual(err, null);
+          assert.equal(err.code, 'CLOUDS_CALL_SERVICE_TIMEOUT');
+          next();
+        });
+      },
+    ], function (err) {
+      console.log(err && err.stack);
+      assert.equal(err, null);
+      Promise.all([c.exitP()]).then(ret => done()).catch(done);
+    });
+  });
+
+  it('test3 - normal call - timeoutChecker=1 - no server', function (done) {
+    const c = clouds.createClient({timeout: 1, timeoutChecker: 1});
+    async.series([
+      function (next) {
+        c.bind('test3.timeout.1');
+        c.ready(next);
+      },
+      function (next) {
+        c.call('test3.timeout.1', [], function (err, ret) {
+          assert.notEqual(err, null);
+          assert.equal(err.code, 'CLOUDS_CALL_SERVICE_TIMEOUT');
+          next();
+        });
+      },
+    ], function (err) {
+      console.log(err && err.stack);
+      assert.equal(err, null);
+      Promise.all([c.exitP()]).then(ret => done()).catch(done);
+    });
+  });
+
   it('test3 - normal bind', function (done) {
     const s = clouds.createServer();
     const c = clouds.createClient({timeout: 1});
